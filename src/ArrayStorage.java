0468a587b6260a26449ec39164d0cd31cc2a3066
Larandvik <0658203@gmail.com>
2024-01-29 16:04:18 -0300

    implemented the delete() and getAll() methods and added field counterResume, modified methods to improve speed and memory retention

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,17 +1,22 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int counterResume;
 
     void clear() {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < counterResume; i++) {
             storage[i] = null;
         }
+        counterResume = 0;
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[counterResume] = r;
+        counterResume++;
     }
 
     Resume get(String uuid) {
@@ -24,30 +29,35 @@ public class ArrayStorage {
                 }
             }
         } catch (NullPointerException e) {
-            System.out.println("Такого резюме нет или база пуста");
+            System.out.println("Такого резюме нет");
         }
-
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < counterResume; i++) {
+            try {
+                if (storage[i].toString().equals(uuid)) {
+                    storage[i] = null;
+                    for (int j = i; j < counterResume; j++) {
+                        storage[j] = storage[j + 1];
+                    }
+                    counterResume--;
+                }
+            } catch (NullPointerException e) {
+                System.out.println("Такого резюме нет");
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, counterResume);
     }
 
     int size() {
-        int size = 0;
-        for (Resume resume : storage) {
-            if (resume == null) {
-                break;
-            }
-            size++;
-        }
-        return size;
+        return counterResume;
     }
 }
