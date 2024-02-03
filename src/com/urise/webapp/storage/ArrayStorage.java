4be43b82c994f94b31037bbc62f0fb5d90d05d78
Larandvik <0658203@gmail.com>
2024-02-03 14:05:00 -0300

    HW 2.1 Сhange access modifiers and code refactor

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,38 +1,38 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.model.Resume;
+
 import java.util.Arrays;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private final Resume[] storage = new Resume[10000];
     private int countResumes;
 
-    void clear() {
-        for (int i = 0; i < countResumes; i++) {
-            storage[i] = null;
-        }
+    public void clear() {
+        Arrays.fill(storage, 0,countResumes, null);
         countResumes = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         storage[countResumes] = r;
         countResumes++;
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < countResumes; i++) {
-            if (storage[i].toString().equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 return storage[i];
             }
         }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         for (int i = 0; i < countResumes; i++) {
-            if (storage[i].toString().equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 storage[i] = storage[--countResumes];
                 storage[countResumes] = null;
                 break;
@@ -43,11 +43,11 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, countResumes);
     }
 
-    int size() {
+    public int size() {
         return countResumes;
     }
 }
