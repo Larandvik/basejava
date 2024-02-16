2d1b9592541e2730699e1dcc16072ca8f671ef35
Larandvik <0658203@gmail.com>
2024-02-16 15:52:35 -0300

    HW 4.1 Add MainReflection

---
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -0,0 +1,27 @@
+package ru.javawebinar.basejava;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+        field.set(r, "new_uuid");
+        System.out.println(r);
+
+        System.out.println("\ninvoke toString");
+        Method method1 = r.getClass().getDeclaredMethod("toString");
+        method1.setAccessible(true);
+        System.out.println(method1.invoke(r));
+        Method method2 = r.getClass().getDeclaredMethods()[1];
+        method2.setAccessible(true);
+        System.out.println(method2.invoke(r));
+    }
+}
