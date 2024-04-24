bd135a9ccc68356a1dcd16cb67afdc9cce0036bf
Larandvik <0658203@gmail.com>
2024-04-24 01:26:23 -0300

    HW 8.5 Recursive method printAllFilesNames()

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -3,6 +3,7 @@ package ru.javawebinar.basejava;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainFile {
     public static void main(String[] args) {
@@ -28,6 +29,20 @@ public class MainFile {
             System.out.println(fis.read());
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+
+        File directory = new File("E:\\IDEA_Project\\basejava\\src");
+        System.out.println("printAllFilesNames\n");
+        printAllFilesNames(directory);
+    }
+
+    private static void printAllFilesNames(File dir) {
+        File[] files = Objects.requireNonNull(dir.listFiles(), "file empty");
+        for (File file : files) {
+            if (file.isDirectory()) {
+                printAllFilesNames(file);
+            }
+            System.out.println(file.getName());
         }
     }
 }
