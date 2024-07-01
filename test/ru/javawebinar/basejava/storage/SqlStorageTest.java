fe6293dcede0cdc62c1a7eb94f9b505c904ab5e9
Larandvik <0658203@gmail.com>
2024-07-01 17:32:03 -0300

    14.9 Add web and delete Maven

--- test/ru/javawebinar/basejava/storage/SqlStorageTest.java
+++ test/ru/javawebinar/basejava/storage/SqlStorageTest.java
@@ -1,16 +1,8 @@
 package ru.javawebinar.basejava.storage;
 
-import org.junit.jupiter.api.AfterAll;
-import ru.javawebinar.basejava.util.ConnectionManager;
-
 class SqlStorageTest extends AbstractStorageTest {
 
     public SqlStorageTest() {
         super(new SqlStorage());
-    }
-
-    @AfterAll
-    public static void tearDown() {
-        ConnectionManager.closePool();
     }
 }
